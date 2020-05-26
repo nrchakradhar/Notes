e40334ad2345a09e313ddfd56a5aa390954b1c0b
Chakradhar N R <38882990+nrchakradhar@users.noreply.github.com>
2020-05-26 19:26:22 +0530

    Add files via upload

---
+++ pythonGrpc/calculator.py
@@ -0,0 +1,8 @@
+import math
+
+def square_root(x):
+  y = math.sqrt(x)
+  return y
+
+if __name__ == "__main__":
+    print(square_root(400))
---
+++ pythonGrpc/grpcClient.py
@@ -0,0 +1,20 @@
+import grpc
+
+# import the generated classes
+import calculator_pb2
+import calculator_pb2_grpc
+
+# open a gRPC channel
+# match this with what was opened in grpcCalcServer.py
+channel = grpc.insecure_channel('localhost:50051')
+
+# create a stub (client)
+# stub is what grpc uses for the term client
+stub = calculator_pb2_grpc.CalculatorStub(channel)
+
+# create a valid request message
+number = calculator_pb2.Number(value=144)
+
+# make the call
+response = stub.SquareRoot(number)
+print(response.value)
